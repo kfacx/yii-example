c27c1cd2229b505dda9200f7a441076062f538cc
Alexander Scott <ascott@rackspace.com>
2013-07-20 08:01:59 -0500

    Genearl cleanup * Added DB connection information. * Created the test-db * Added the GUIDLINES file.

--- example/protected/views/site/index.php
+++ example/protected/views/site/index.php
@@ -4,15 +4,9 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<p>This example is based on Yii version <? echo Yii::getVersion() ?>.</p>
 
 <p>For more details on how to further develop this application, please read
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.

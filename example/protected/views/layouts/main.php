37a9ada38e5fbd460af1283dadd0b3ae5c21b41f
Alexander Scott <ascott@rackspace.com>
2013-07-22 09:11:23 -0500

    Added the Basic Form route to the menu

--- example/protected/views/layouts/main.php
+++ example/protected/views/layouts/main.php
@@ -30,6 +30,7 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'Basic Form', 'url'=>array('/basicform/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),

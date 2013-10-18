ebf235248a4b5d344dcf65d48efe6ff7235860df
Alexander Scott <ascott@rackspace.com>
2013-10-18 15:50:38 -0500

    Backported puppet updates from other projects

--- puppet/modules/phpmyadmin/manifests/init.pp
+++ puppet/modules/phpmyadmin/manifests/init.pp
@@ -3,33 +3,33 @@
 # All rights reserved.
 #
 class phpmyadmin ($db_root_pw) {
-  class {
-    'mysql': db_root_pw => $db_root_pw;
-  }
-  include apache2
-  include php
+	class {
+		'mysql': db_root_pw => $db_root_pw;
+	}
+	include apache2
+	include php
 
-  file {
-    "/root/phpmyadmin.preseed":
-      ensure => present,
-      mode => 0600,
-      content => template("phpmyadmin/preseed.erb");
-  }
+	file {
+		"/root/phpmyadmin.preseed":
+			ensure => present,
+			mode => 0600,
+			content => template("phpmyadmin/preseed.erb");
+	}
 
-  exec {
-    "preseed_phpmyadmin":
-      path => ["/bin", "/usr/bin"],
-      command => "debconf-set-selections < /root/phpmyadmin.preseed",
-      unless => "debconf-get-selections | grep phpmyadmin",
-      require => [ File["/root/phpmyadmin.preseed"], Package["debconf-utils"] ],
-      logoutput => "on_failure";
-  }
+	exec {
+		"preseed_phpmyadmin":
+			path => ["/bin", "/usr/bin"],
+			command => "debconf-set-selections < /root/phpmyadmin.preseed",
+			unless => "debconf-get-selections | grep phpmyadmin",
+			require => [ File["/root/phpmyadmin.preseed"], Package["debconf-utils"] ],
+			logoutput => "on_failure";
+	}
 
-  package {
-    "debconf-utils":
-      ensure => present;
-    "phpmyadmin":
-      ensure => present,
-      require => [ Package["php5", "debconf-utils"], Service["mysql", "apache2"], Exec["preseed_phpmyadmin"] ];
-  }
+	package {
+		"debconf-utils":
+			ensure => present;
+		"phpmyadmin":
+			ensure => present,
+			require => [ Package["php5", "debconf-utils"], Service["mysql", "apache2"], Exec["preseed_phpmyadmin"] ];
+	}
 }
